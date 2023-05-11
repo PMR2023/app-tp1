6b148d157733e1c1ecc3de44e1e29726fcac2ccc
Thomas Bourdeaud'huy <thomas.bourdeaudhuy@gmail.com>
2023-05-11 11:47:49 +0200

    Onclicklistener anonyme

--- app/src/main/java/com/example/apptp1/MainActivity.java
+++ app/src/main/java/com/example/apptp1/MainActivity.java
@@ -4,16 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CAT = "PMR";
+    private EditText edtPseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(CAT, "onCreate");
+        edtPseudo = findViewById(R.id.edtPseudo);
+        edtPseudo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saluer("entrez votre pseudo");
+            }
+        });
+    }
+    public void saluer(String s) {
+        Toast myToast = Toast.makeText(this,s,Toast.LENGTH_LONG);
+        myToast.show();
+        Log.i(CAT, s);
     }
 
     @Override
@@ -21,4 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Log.i(CAT, "onStart");
     }
+
+
 }
